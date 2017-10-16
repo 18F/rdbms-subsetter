dff49dd6f2e4f5c29cee4a0b0cec2dc9c91fb1c5
Catherine Devlin <catherine.devlin@gsa.gov>
2017-10-16 14:29:24 -0400

    Switched to py.test

--- tests/test_subsetter.py
+++ tests/test_subsetter.py
@@ -1,8 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Tests for `sql_insert_writer` package."""
+
 import os
-import unittest
-import tempfile
+import subprocess
 import sqlite3
+import tempfile
+
+import pytest
+import pytest_postgresql
+
 from subsetter import Db
+
+try:
+    subprocess.check_output('command -v pg_ctl', shell=True)
+    PG_CTL_MISSING = False  # sorry for the double-negative, but it's convenient later
+except subprocess.CalledProcessError:
+    PG_CTL_MISSING = True
+
+
+TABLE_DEFINITIONS = ["CREATE TABLE state (abbrev, name)",
+                  "CREATE TABLE zeppos (name, home_city)",
+                  """CREATE TABLE city (name, state_abbrev,
+                                        FOREIGN KEY (state_abbrev)
+                                        REFERENCES state(abbrev))""",
+                  """CREATE TABLE landmark (name, city,
+                                            FOREIGN KEY (city)
+                                            REFERENCES city(name))""",
+                  """CREATE TABLE zeppelins (name, home_city,
+                                             FOREIGN KEY (home_city)
+                                             REFERENCES city(name))""", # NULL FKs
+                  """CREATE TABLE languages_better_than_python (name)""", # empty table
+]
 
 class DummyArgs(object):
     logarithmic = False
@@ -17,134 +46,142 @@ class DummyArgs(object):
 
 dummy_args = DummyArgs()
 
-class OverallTest(unittest.TestCase):
+@pytest.mark.skip
+# @pytest.mark.skipif(PG_CTL_MISSING, reason='PostgreSQL not installed locally')
+def pg_data(postgresql):
+    pytest.set_trace()
+    for table_def in TABLE_DEFINITIONS:
+        pass
 
-    def setUp(self):
-        schema = ["CREATE TABLE state (abbrev, name)",
-                  "CREATE TABLE zeppos (name, home_city)",
-                  """CREATE TABLE city (name, state_abbrev,
-                                        FOREIGN KEY (state_abbrev)
-                                        REFERENCES state(abbrev))""",
-                  """CREATE TABLE landmark (name, city,
-                                            FOREIGN KEY (city)
-                                            REFERENCES city(name))""",
-                  """CREATE TABLE zeppelins (name, home_city,
-                                             FOREIGN KEY (home_city)
-                                             REFERENCES city(name))""", # NULL FKs
-                  """CREATE TABLE languages_better_than_python (name)""", # empty table
-                  ]
-        self.source_db_filename = tempfile.mktemp()
-        self.source_db = sqlite3.connect(self.source_db_filename)
-        self.source_sqla = "sqlite:///%s" % self.source_db_filename
-        self.dest_db_filename = tempfile.mktemp()
-        self.dest_db = sqlite3.connect(self.dest_db_filename)
-        self.dest_sqla = "sqlite:///%s" % self.dest_db_filename
-        for statement in schema:
-            self.source_db.execute(statement)
-            self.dest_db.execute(statement)
-        for params in (('MN', 'Minnesota'), ('OH', 'Ohio'),
-                       ('MA', 'Massachussetts'), ('MI', 'Michigan')):
-            self.source_db.execute("INSERT INTO state VALUES (?, ?)", params)
-        for params in (('Duluth', 'MN'), ('Dayton', 'OH'),
-                       ('Boston', 'MA'), ('Houghton', 'MI')):
-            self.source_db.execute("INSERT INTO city VALUES (?, ?)", params)
-        for params in (('Lift Bridge', 'Duluth'), ("Mendelson's", 'Dayton'),
-                       ('Trinity Church', 'Boston'), ('Michigan Tech', 'Houghton')):
-            self.source_db.execute("INSERT INTO landmark VALUES (?, ?)", params)
-        for params in (('Graf Zeppelin', None), ('USS Los Angeles', None),
-                       ('Nordstern', None), ('Bodensee', None)):
-            self.source_db.execute("INSERT INTO zeppelins VALUES (?, ?)", params)
-        for params in (('Zeppo Marx', 'New York City'), ):
-            self.source_db.execute("INSERT INTO zeppos VALUES (?, ?)", params)
-        self.source_db.commit()
-        self.dest_db.commit()
+def temp_sqlite_db():
+    """Creates a temporary sqlite database file
 
-    def tearDown(self):
-        self.source_db.close()
-        os.unlink(self.source_db_filename)
-        self.dest_db.close()
-        os.unlink(self.dest_db_filename)
+    Return (filename, connection)"""
 
-    def test_parents_kept(self):
-        src = Db(self.source_sqla, dummy_args)
-        dest = Db(self.dest_sqla, dummy_args)
-        src.assign_target(dest)
-        src.create_subset_in(dest)
-        cities = self.dest_db.execute("SELECT * FROM city").fetchall()
-        self.assertEqual(len(cities), 1)
-        joined = self.dest_db.execute("""SELECT c.name, s.name
-                                         FROM city c JOIN state s
-                                                     ON (c.state_abbrev = s.abbrev)""")
-        joined = joined.fetchall()
-        self.assertEqual(len(joined), 1)
+    filename = tempfile.mktemp()
+    db = sqlite3.connect(filename)
+    return (filename, db)
 
-    def test_null_foreign_keys(self):
-        src = Db(self.source_sqla, dummy_args)
-        dest = Db(self.dest_sqla, dummy_args)
-        src.assign_target(dest)
-        src.create_subset_in(dest)
-        zeppelins = self.dest_db.execute("SELECT * FROM zeppelins").fetchall()
-        self.assertEqual(len(zeppelins), 1)
+def sqla_url(filename):
+    return "sqlite:///%s" % filename
 
-    def test_tables(self):
-        args_with_tables = DummyArgs()
-        args_with_tables.tables = ['state', 'city']
-        src = Db(self.source_sqla, args_with_tables)
-        dest = Db(self.dest_sqla, args_with_tables)
-        src.assign_target(dest)
-        src.create_subset_in(dest)
-        states = self.dest_db.execute("SELECT * FROM state").fetchall()
-        self.assertEqual(len(states), 1)
-        cities = self.dest_db.execute("SELECT * FROM city").fetchall()
-        self.assertEqual(len(cities), 1)
-        excluded_tables = ('landmark', 'languages_better_than_python', 'zeppos', 'zeppelins')
-        for table in excluded_tables:
-            rows = self.dest_db.execute("SELECT * FROM {}".format(table)).fetchall()
-            self.assertEqual(len(rows), 0)
+def fetch_from(db, sql):
+    curs = db.cursor()
+    curs.execute(sql)
+    return curs.fetchall()
 
-    def test_exclude_tables(self):
-        args_with_exclude = DummyArgs()
-        args_with_exclude.exclude_tables = ['zeppelins',]
-        src = Db(self.source_sqla, args_with_exclude)
-        dest = Db(self.dest_sqla, args_with_exclude)
-        src.assign_target(dest)
-        src.create_subset_in(dest)
-        zeppelins = self.dest_db.execute("SELECT * FROM zeppelins").fetchall()
-        self.assertEqual(len(zeppelins), 0)
+def insert_data(curs):
+    for params in (('MN', 'Minnesota'), ('OH', 'Ohio'),
+                    ('MA', 'Massachussetts'), ('MI', 'Michigan')):
+        curs.execute("INSERT INTO state VALUES (?, ?)", params)
+    for params in (('Duluth', 'MN'), ('Dayton', 'OH'),
+                    ('Boston', 'MA'), ('Houghton', 'MI')):
+        curs.execute("INSERT INTO city VALUES (?, ?)", params)
+    for params in (('Lift Bridge', 'Duluth'), ("Mendelson's", 'Dayton'),
+                    ('Trinity Church', 'Boston'), ('Michigan Tech', 'Houghton')):
+        curs.execute("INSERT INTO landmark VALUES (?, ?)", params)
+    for params in (('Graf Zeppelin', None), ('USS Los Angeles', None),
+                    ('Nordstern', None), ('Bodensee', None)):
+        curs.execute("INSERT INTO zeppelins VALUES (?, ?)", params)
+    for params in (('Zeppo Marx', 'New York City'), ):
+        curs.execute("INSERT INTO zeppos VALUES (?, ?)", params)
 
-    def test_tables_and_exclude_tables(self):
-        args_with_tables_and_exclude_tables = DummyArgs()
-        args_with_tables_and_exclude_tables.tables = ['state', 'city']
-        args_with_tables_and_exclude_tables.exclude_tables = ['city']
-        src = Db(self.source_sqla, args_with_tables_and_exclude_tables)
-        dest = Db(self.dest_sqla, args_with_tables_and_exclude_tables)
-        src.assign_target(dest)
-        src.create_subset_in(dest)
-        states = self.dest_db.execute("SELECT * FROM state").fetchall()
-        self.assertEqual(len(states), 1)
-        excluded_tables = ('city', 'landmark', 'languages_better_than_python', 'zeppos', 'zeppelins')
-        for table in excluded_tables:
-            rows = self.dest_db.execute("SELECT * FROM {}".format(table)).fetchall()
-            self.assertEqual(len(rows), 0)
+@pytest.fixture
+def sqlite_data(request):
+    (source_filename, source_db) = temp_sqlite_db()
+    (dest_filename, dest_db) = temp_sqlite_db()
 
-    def test_full_tables(self):
-        args_with_full = DummyArgs()
-        args_with_full.full_tables = ['city',]
-        src = Db(self.source_sqla, args_with_full)
-        dest = Db(self.dest_sqla, args_with_full)
-        src.assign_target(dest)
-        src.create_subset_in(dest)
-        cities = self.dest_db.execute("SELECT * FROM city").fetchall()
-        self.assertEqual(len(cities), 4)
+    source_curs = source_db.cursor()
+    dest_curs = dest_db.cursor()
+    for table_def in TABLE_DEFINITIONS:
+        source_curs.execute(table_def)
+        dest_curs.execute(table_def)
+    insert_data(source_curs)
+    source_db.commit()
+    dest_db.commit()
 
-    def test_exclude_tables_wildcard(self):
-        args_with_exclude = DummyArgs()
-        args_with_exclude.exclude_tables = ['zep*',]
-        src = Db(self.source_sqla, args_with_exclude)
-        dest = Db(self.dest_sqla, args_with_exclude)
-        src.assign_target(dest)
-        src.create_subset_in(dest)
-        zeppelins = self.dest_db.execute("SELECT * FROM zeppelins").fetchall()
-        self.assertEqual(len(zeppelins), 0)
-        zeppos = self.dest_db.execute("SELECT * FROM zeppos").fetchall()
-        self.assertEqual(len(zeppos), 0)
+    yield (sqla_url(source_filename),
+           sqla_url(dest_filename))
+
+    source_db.close()
+    os.unlink(source_filename)
+    dest_db.close()
+    os.unlink(dest_filename)
+
+def results(src_url, dest_url, arguments):
+    src = Db(src_url, arguments)
+    dest = Db(dest_url, arguments)
+    src.assign_target(dest)
+    src.create_subset_in(dest)
+    return (src, dest)
+
+def test_parents_kept(sqlite_data):
+    (src, dest) = results(*sqlite_data, dummy_args)
+    dest_curs = dest.conn.connection.cursor()
+    cities = dest_curs.execute("SELECT * FROM city").fetchall()
+    assert len(cities) == 1
+    joined = dest_curs.execute("""SELECT c.name, s.name
+                                    FROM city c JOIN state s
+                                    ON (c.state_abbrev = s.abbrev)""").fetchall()
+    assert len(joined) == 1
+
+
+def test_null_foreign_keys(sqlite_data):
+    (src, dest) = results(*sqlite_data, dummy_args)
+    dest_curs = dest.conn.connection.cursor()
+    zeppelins = dest_curs.execute("SELECT * FROM zeppelins").fetchall()
+    assert len(zeppelins) == 1
+
+def test_tables_arg(sqlite_data):
+    args_with_tables = DummyArgs()
+    args_with_tables.tables = ['state', 'city']
+    (src, dest) = results(*sqlite_data, args_with_tables)
+    dest_curs = dest.conn.connection.cursor()
+    states = dest_curs.execute("SELECT * FROM state").fetchall()
+    assert len(states) == 1
+    cities = dest_curs.execute("SELECT * FROM city").fetchall()
+    assert len(cities) == 1
+    excluded_tables = ('landmark', 'languages_better_than_python', 'zeppos', 'zeppelins')
+    for table in excluded_tables:
+        rows = dest_curs.execute("SELECT * FROM {}".format(table)).fetchall()
+        assert not rows
+
+def test_exclude_tables(sqlite_data):
+    args_with_exclude = DummyArgs()
+    args_with_exclude.exclude_tables = ['zeppelins',]
+    (src, dest) = results(*sqlite_data, args_with_exclude)
+    dest_curs = dest.conn.connection.cursor()
+    zeppelins = dest_curs.execute("SELECT * FROM zeppelins").fetchall()
+    assert not zeppelins
+
+
+def test_tables_and_exclude_tables(sqlite_data):
+    args_with_tables_and_exclude_tables = DummyArgs()
+    args_with_tables_and_exclude_tables.tables = ['state', 'city']
+    args_with_tables_and_exclude_tables.exclude_tables = ['city']
+    (src, dest) = results(*sqlite_data, args_with_tables_and_exclude_tables)
+    dest_curs = dest.conn.connection.cursor()
+    states = dest_curs.execute("SELECT * FROM state").fetchall()
+    assert len(states) == 1
+    excluded_tables = ('city', 'landmark', 'languages_better_than_python', 'zeppos', 'zeppelins')
+    for table in excluded_tables:
+        rows = dest_curs.execute("SELECT * FROM {}".format(table)).fetchall()
+        assert not rows
+
+def test_full_tables(sqlite_data):
+    args_with_full = DummyArgs()
+    args_with_full.full_tables = ['city',]
+    (src, dest) = results(*sqlite_data, args_with_full)
+    dest_curs = dest.conn.connection.cursor()
+    cities = dest_curs.execute("SELECT * FROM city").fetchall()
+    assert len(cities) == 4
+
+def test_exclude_tables_wildcard(sqlite_data):
+    args_with_exclude = DummyArgs()
+    args_with_exclude.exclude_tables = ['zep*',]
+    (src, dest) = results(*sqlite_data, args_with_exclude)
+    dest_curs = dest.conn.connection.cursor()
+    zeppelins = dest_curs.execute("SELECT * FROM zeppelins").fetchall()
+    assert not zeppelins
+    zeppos = dest_curs.execute("SELECT * FROM zeppos").fetchall()
+    assert not zeppos
